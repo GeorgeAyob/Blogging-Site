3207ca9c19d6e5e0a3caba2d4a1f64fc9908d8e4
George Ayob <51719874+GeorgeAyob@users.noreply.github.com>
2020-03-03 15:40:30 -0500

    Update resultsBook.php

--- resultsBook.php
+++ resultsBook.php
@@ -5,9 +5,9 @@
 
 echo "<h1> BOOK RESULTS</h1>";
 
-define('DB_HOST','localhost');
-define('DB_USER','ayob');
-define('DB_PASSWORD','Whosays1');
+define('DB_HOST','');
+define('DB_USER','');
+define('DB_PASSWORD','');
 define('DB_NAME','bookOrama');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die("Could not connect to database".mysqli_connect_error());
